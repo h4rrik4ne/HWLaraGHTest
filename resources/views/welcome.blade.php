c5bb1b2d00a022c8bfa809a5c12cef89c36a2b9d
Harry <test@test.de>
2018-11-27 17:02:44 +0100

    Second Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HWLaraGHTest
+                    HWLaraGHTest: Nächster commit
                 </div>
 
                 <div class="links">
